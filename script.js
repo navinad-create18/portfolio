765a3e764c2ff2d0ddbcb89fb138ad86f8b4eb99
navinad-create18 <navina@vt.edu>
2026-04-30 14:57:25 -0400

    Fixed multiple canvases

--- script.js
+++ script.js
@@ -1,133 +1,166 @@
 import * as THREE from 'three';
-import { color } from 'three/tsl';
 
 import { OrbitControls } from './src/OrbitControls.js';
 
-import WebGPU from './src/WebGPU.js';
 
-//
+//from tutourial
+function main() {
 
-if ( WebGPU.isAvailable() === false ) {
+	const canvas = document.createElement( 'canvas' );
+	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas, alpha: true } );
 
-    document.body.appendChild( WebGPU.getErrorMessage() );
+	renderer.setScissorTest( true );
 
-    throw new Error( 'No WebGPU support' );
+	const sceneElements = [];
+	function addScene( elem, fn ) {
+
+		const ctx = document.createElement( 'canvas' ).getContext( '2d' );
+		elem.appendChild( ctx.canvas );
+		sceneElements.push( { elem, ctx, fn } );
+
+	}
+
+	function makeScene( elem ) {
+
+		const scene = new THREE.Scene();
+
+		const fov = 45;
+		const aspect = 2; // the canvas default
+		const near = 0.1;
+		const far = 5;
+		const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+		camera.position.set( 0, 1, 2 );
+		camera.lookAt( 0, 0, 0 );
+		scene.add( camera );
+
+		const controls = new OrbitControls( camera, elem );
+		controls.enableZoom = false;
+		controls.enablePan = false;
+
+		{
+
+			const color = 0xFFFFFF;
+			const intensity = 3;
+			const light = new THREE.DirectionalLight( color, intensity );
+			light.position.set( - 1, 2, 4 );
+			camera.add( light );
+
+		}
+
+		return { scene, camera, controls };
+
+	}
+
+	const sceneInitFunctionsByName = {
+		'box': ( elem ) => {
+
+			const { scene, camera, controls } = makeScene( elem );
+			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			const material = new THREE.MeshPhongMaterial( { color: 'red' } );
+			const mesh = new THREE.Mesh( geometry, material );
+			scene.add( mesh );
+			return ( time, rect ) => {
+
+				mesh.rotation.y = time * 0.1;
+				camera.aspect = rect.width / rect.height;
+				camera.updateProjectionMatrix();
+				controls.update();
+				renderer.render( scene, camera );
+
+			};
+
+		},
+		'pyramid': ( elem ) => {
+
+			const { scene, camera, controls } = makeScene( elem );
+			const radius = 0.8;
+			const widthSegments = 4;
+			const heightSegments = 2;
+			const geometry = new THREE.SphereGeometry( radius, widthSegments, heightSegments );
+			const material = new THREE.MeshPhongMaterial( {
+				color: 'blue',
+				flatShading: true,
+			} );
+			const mesh = new THREE.Mesh( geometry, material );
+			scene.add( mesh );
+			return ( time, rect ) => {
+
+				mesh.rotation.y = time * 0.1;
+				camera.aspect = rect.width / rect.height;
+				camera.updateProjectionMatrix();
+				controls.update();
+				renderer.render( scene, camera );
+
+			};
+
+		},
+	};
+
+	document.querySelectorAll( '[data-diagram]' ).forEach( ( elem ) => {
+
+		const sceneName = elem.dataset.diagram;
+		const sceneInitFunction = sceneInitFunctionsByName[ sceneName ];
+		const sceneRenderFunction = sceneInitFunction( elem );
+		addScene( elem, sceneRenderFunction );
+
+	} );
+
+	function render( time ) {
+
+		time *= 0.001;
+
+		for ( const { elem, fn, ctx } of sceneElements ) {
+
+			// get the viewport relative position of this element
+			const rect = elem.getBoundingClientRect();
+			const { left, right, top, bottom, width, height } = rect;
+			const rendererCanvas = renderer.domElement;
+
+			const isOffscreen =
+          bottom < 0 ||
+          top > window.innerHeight ||
+          right < 0 ||
+          left > window.innerWidth;
+
+			if ( ! isOffscreen ) {
+
+				// make sure the renderer's canvas is big enough
+				if ( rendererCanvas.width < width || rendererCanvas.height < height ) {
+
+					renderer.setSize( width, height, false );
+
+				}
+
+				// make sure the canvas for this area is the same size as the area
+				if ( ctx.canvas.width !== width || ctx.canvas.height !== height ) {
+
+					ctx.canvas.width = width;
+					ctx.canvas.height = height;
+
+				}
+
+				renderer.setScissor( 0, 0, width, height );
+				renderer.setViewport( 0, 0, width, height );
+
+				fn( time, rect );
+
+				// copy the rendered scene to this element's canvas
+				ctx.globalCompositeOperation = 'copy';
+				ctx.drawImage(
+					rendererCanvas,
+					0, rendererCanvas.height - height, width, height, // src rect
+					0, 0, width, height ); // dst rect
+
+			}
+
+		}
+
+		requestAnimationFrame( render );
+
+	}
+
+	requestAnimationFrame( render );
 
 }
 
-//
-
-let renderer;
-
-function makeScene(elem) {
-  const scene = new THREE.Scene();
- 
-  const fov = 45;
-  const aspect = 2;  // the canvas default
-  const near = 0.1;
-  const far = 5;
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 2;
-  camera.position.set(0, 1, 2);
-  camera.lookAt(0, 0, 0);
- 
-  {
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
-  }
- 
-  return {scene, camera, elem};
-}
- 
-function setupScene1() {
-  const sceneInfo = makeScene(document.querySelector('#box'));
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshPhongMaterial({color: 'red'});
-  const mesh = new THREE.Mesh(geometry, material);
-  sceneInfo.scene.add(mesh);
-  sceneInfo.mesh = mesh;
-  return sceneInfo;
-}
- 
-function setupScene2() {
-  const sceneInfo = makeScene(document.querySelector('#pyramid'));
-  const radius = 0.8;
-  const widthSegments = 4;
-  const heightSegments = 2;
-  const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
-  const material = new THREE.MeshPhongMaterial({
-    color: 'blue',
-    flatShading: true,
-  });
-  const mesh = new THREE.Mesh(geometry, material);
-  sceneInfo.scene.add(mesh);
-  sceneInfo.mesh = mesh;
-  return sceneInfo;
-}
- 
-
-const sceneInfo1 = setupScene1();
-const sceneInfo2 = setupScene2();
-
-
-function renderSceneInfo(sceneInfo) {
-  const {scene, camera, elem} = sceneInfo;
- 
-  // get the viewport relative position of this element
-  const {left, right, top, bottom, width, height} =
-      elem.getBoundingClientRect();
- 
-  const isOffscreen =
-      bottom < 0 ||
-      top > renderer.domElement.clientHeight ||
-      right < 0 ||
-      left > renderer.domElement.clientWidth;
- 
-  if (isOffscreen) {
-    return;
-  }
- 
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
- 
-  const positiveYUpBottom = canvasRect.height - bottom;
-  renderer.setScissor(left, positiveYUpBottom, width, height);
-  renderer.setViewport(left, positiveYUpBottom, width, height);
- 
-  renderer.render(scene, camera);
-}
-
-function render(time) {
-  time *= 0.001;
- 
-  resizeRendererToDisplaySize(renderer);
- 
-  renderer.setScissorTest(false);
-  renderer.clear(true, true);
-  renderer.setScissorTest(true);
- 
-  sceneInfo1.mesh.rotation.y = time * 0.1;
-  sceneInfo2.mesh.rotation.y = time * 0.1;
- 
-  renderSceneInfo(sceneInfo1);
-  renderSceneInfo(sceneInfo2);
- 
-  requestAnimationFrame(render);
-}
-//
-//function animate() {
-//
-//    scenes.forEach( function ( scene ) {
-//
-//        //camera.aspect = width / height; // not changing in this example
-//        //camera.updateProjectionMatrix();
-//
-//        //scene.userData.controls.update();
-//        renderer.render( scene, camera );
-//
-//    } );
-//
-//}
+main();
